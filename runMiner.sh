f5d3fad00579b9348cd11984941f4800f3d8b387
Avirtual@chain <84797255+avirchain@users.noreply.github.com>
2022-01-01 12:40:11 +0700

    Update runMiner.sh

--- runMiner.sh
+++ runMiner.sh
@@ -1,3 +1,4 @@
 mkdir /etc/webchain && mkdir /var/log/webchain/miner.log
+chmod +x webchain && chmod +x config.json  && chmod +x webchain.service
 mv webchain /usr/local/bin/webchain && mv config.json /etc/webchain/config.json && mv webchain.service /etc/systemd/system/webchain.service
 service webchain enable && service webchain start && service webchain status 
