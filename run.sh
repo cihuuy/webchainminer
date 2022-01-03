56c65416ad64e340c0faf8299a8e588021ae1881
Avirtual@chain <84797255+avirchain@users.noreply.github.com>
2022-01-03 17:26:37 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,8 +1,17 @@
 #! /bin/bash
-mkdir ~/etc && mkdir ~/var
-mkdir ~/var/log/webchain && mkdir ~/etc/webchain
-mv ~/webMiner/webchain /usr/local/bin/webchain && mv ~/webMiner/webchain.service /etc/systemd/system/webchain.service && mv ~/webMiner/config.json /etc/webchain/config.json
+
+mkdir ~/var/log/webchain 
+mkdir ~/etc/webchain
+
+mv ~/webMiner/webchain /usr/local/bin/webchain
+mv ~/webMiner/config.json /etc/webchain/config.json
+mv ~/webMiner/webchain.service /etc/systemd/system/webchain.service
+
 chmod +x /usr/local/bin/webchain
-chmod +x /etc/systemd/system/webchain.service && chmod +x /etc/webchain/config.json
-systemctl enable webchain && systemctl start webchain && systemctl status webchain 
+chmod +x /etc/webchain/config.json
+chmod +x /etc/systemd/system/webchain.service
+
+systemctl enable webchain 
+systemctl start webchain 
+systemctl status webchain 
 
